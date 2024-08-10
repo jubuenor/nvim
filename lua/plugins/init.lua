5a8f379d9dcabba2d4c1391e68c3d34be4ed6db9
jubuenor <jubuenor@unal.edu.co>
2024-08-10 15:52:28 -0500

    java

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,7 +3,7 @@ return {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
     config = function()
-      require("configs.conform")
+      require "configs.conform"
     end,
   },
 
@@ -13,7 +13,7 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       require("nvchad.configs.lspconfig").defaults()
-      require("configs.lspconfig")
+      require "configs.lspconfig"
     end,
   },
   --
@@ -41,7 +41,7 @@ return {
     cmd = "Copilot",
     event = "InsertEnter",
     config = function()
-      require("configs.copilot")
+      require "configs.copilot"
     end,
     -- lazy = false,
   },
@@ -71,7 +71,37 @@ return {
     },
     lazy = false,
     config = function()
-      require("configs.autoSession")
+      require "configs.autoSession"
+    end,
+  },
+  {
+    "nvim-java/nvim-java",
+    lazy = false,
+    dependencies = {
+      "nvim-java/lua-async-await",
+      "nvim-java/nvim-java-core",
+      "nvim-java/nvim-java-test",
+      "nvim-java/nvim-java-dap",
+      "MunifTanjim/nui.nvim",
+      "neovim/nvim-lspconfig",
+      "mfussenegger/nvim-dap",
+      {
+        "williamboman/mason.nvim",
+        opts = {
+          registries = {
+            "github:nvim-java/mason-registry",
+            "github:mason-org/mason-registry",
+          },
+        },
+      },
+    },
+    config = function()
+      require("java").setup {}
+      require("lspconfig").jdtls.setup {
+        on_attach = require("nvchad.configs.lspconfig").on_attach,
+        capabilities = require("nvchad.configs.lspconfig").capabilities,
+        filetypes = { "java" },
+      }
     end,
   },
 }
