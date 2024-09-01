3ba6819ae848d49488c0ef0cfb4f47e9da781730
jubuenor <jubuenor@unal.edu.co>
2024-09-01 18:59:24 -0500

    float term size

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,16 +1,28 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 
 M.ui = {
-	theme = "onedark",
+  -- theme = "onedark",
+  theme = "yoru",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
+}
+
+M.term = {
+  float = {
+    relative = "editor",
+    border = "single",
+    row = 0.1,
+    col = 0.1,
+    width = 0.8,
+    height = 0.7,
+  },
 }
 
 return M
